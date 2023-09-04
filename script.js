7464e0861377af275b695d0be353b393b3df49ed
RedBeam195 <shadowpilar195@gmail.com>
2023-09-04 13:32:04 +0200

    Scripted modal and form gathering

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const myLibrary = []
+const myLibrary = [];
+let currentBook = [];
 
-function Book(title,author,pages,read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 
-    function getReadStatus(){
-        return this.read ? "green" : "red";
-    }
+  function getReadStatus() {
+    return this.read ? "green" : "red";
+  }
 }
 
-const addBtn = document.querySelector("#add")
+const titleField = document.querySelector("#title");
+const authorField = document.querySelector("#author");
+const pagesField = document.querySelector("#pages");
+const readField = document.querySelector("#read");
 
-addBtn.addEventListener("click",(e)=>{
-    
-})
+const bookCache = (book) => {
+  for (let i = 0; i < 4; i++) {
+    book.pop();
+  }
+  book.push(titleField.value);
+  book.push(authorField.value);
+  book.push(pagesField.value);
+  book.push(readField.checked);
+};
+
+const addBtn = document.querySelector("#add");
+
+addBtn.addEventListener("click", (e) => {
+  document.querySelector(".modal").classList.toggle("shown");
+});
+
+document.querySelector(".close").addEventListener("click", (e) => {
+  document.querySelector(".modal").classList.toggle("shown");
+});
+
+document.querySelector("#register").addEventListener("click", (e) => {
+  document.querySelector(".modal").classList.toggle("shown");
+  bookCache(currentBook);
+  console.log(currentBook);
+});
